9a67148291c60de0a53c5f7629d90770e316aa8e
phamtronghieu <phamtronghieu2002iuh@gmail.com>
2023-08-24 16:10:29 +0700

    fix models

--- models/booking.js
+++ models/booking.js
@@ -6,9 +6,7 @@ module.exports = (sequelize, DataTypes) => {
   class Booking extends Model {
 
     static associate(models) {
-        Booking.belongsTo(models.Allcode,{foreignKey:'statusId'});    
-        Booking.belongsTo(models.User,{foreignKey:'doctorId'});      
-        Booking.belongsTo(models.User,{foreignKey:'patientId'});   
+
     }
   }
   Booking.init({
